9dc6ba2bd8b6dc10ef5a0a1d46e833266381c410
liznomura <lizn@hawaii.edu>
2017-07-26 10:25:33 -1000

    starships

--- js/app.js
+++ js/app.js
@@ -12,53 +12,15 @@ const swapiPeople = 'http://swapi.co/api/people/';
 const swapiPlanets = 'http://swapi.co/api/planets/';
 const swapiStarships = 'http://swapi.co/api/starships/';
 
-// function planetsReq() {
-//     //name in h2
-//     //terrain in p
-//     //population in p
-
-//     let planetsObj = JSON.parse(this.responseText);
-
-//     const planetsName = document.createElement('h2');
-//     planetsName.innerHTML = planetsObj.name;
-
-//     const planetsTerrain = document.createElement('p');
-//     planetsTerrain.innerHTML = planetsObj.terrain;
-
-//     const planetsPopulation = document.createElement('p');
-//     planetsPopulation.innerHTML = planetsObj.population;
-
-//     const planetContainer = document.createElement('div');
-
-//     const planetFilmUl = document.createElement('ul');
-
-//     //film names in li wrapped in ul
-//     for(let i = 0; i < planetsObj.films.length; i++) {
-//       startXHR(HTTP_METHOD.GET, planetsObj[i].films, function() {
-//         let filmObj = JSON.parse(this.responseText);
-//         const planetFilm = document.createElement('li');
-//         planetFilm.innerHTML = filmObj.name;
-//         contentDiv.appendChild(planetsName);
-//         contentDiv.appendChild(planetsTerrain);
-//         contentDiv.appendChild(planetsPopulation);
-//         contentDiv.appendChild(planetFilm);
-//         if(contentDiv.children.length === 0) {
-//           contentDiv.appendChild(planetContainer);
-//         } else {
-//           contentDiv.replaceChild(planetContainer, contentDiv.children[0]);
-//         }
-//       });
-//     }
-//   }
 
 reqBtn.addEventListener('click', function() {
   if(dropDown.value === 'people') {
 
     fetch(swapiPeople + input.value)
-    .then((res) => {
+    .then( res => {
       return res.json();
     })
-    .then((data) => {
+    .then( data => {
     // name in h2
     // gender in p
     // first species value in a p
@@ -73,10 +35,10 @@ reqBtn.addEventListener('click', function() {
     peopleContainer.appendChild(peopleGender);
 
     fetch(data.species)
-    .then((res) => {
+    .then( res => {
       return res.json();
     })
-    .then((data) => {
+    .then( data => {
       const peopleSpecies = document.createElement('p');
       peopleSpecies.innerHTML = data.name;
       peopleContainer.appendChild(peopleSpecies);
@@ -87,9 +49,7 @@ reqBtn.addEventListener('click', function() {
     } else {
       contentDiv.replaceChild(peopleContainer, contentDiv.children[0]);
     }
-
   });
-
 
   } else if (dropDown.value === 'planets') {
     //name in h2
@@ -99,10 +59,10 @@ reqBtn.addEventListener('click', function() {
     const planetContainer = document.createElement('div');
 
     fetch(swapiPlanets + input.value)
-    .then((res) => {
+    .then( res => {
       return res.json();
     })
-    .then((data) => {
+    .then( data => {
       const planetsName = document.createElement('h2');
       planetsName.innerHTML = data.name;
 
@@ -121,15 +81,14 @@ reqBtn.addEventListener('click', function() {
 
       data.films.forEach( film => {
         fetch(film)
-        .then((res) => { return res.json(); })
-        .then((data) => {
+        .then( res => { return res.json(); })
+        .then( data => {
           const planetFilm = document.createElement('li');
           planetFilm.innerHTML = data.title;
           planetContainer.appendChild(planetFilm);
         });
 
         if(contentDiv.children.length === 0) {
-          console.log(contentDiv.children.length);
           contentDiv.appendChild(planetContainer);
         } else {
           contentDiv.replaceChild(planetContainer, contentDiv.children[0]);
@@ -142,5 +101,46 @@ reqBtn.addEventListener('click', function() {
     //manufacturer in p
     //starship class in p
     //film names in li wrapped in ul
+
+    fetch(swapiStarships + input.value)
+    .then( res => {
+      return res.json();
+    })
+    .then( data => {
+      starshipsContainer = document.createElement('div');
+      starshipsName = document.createElement('h2');
+      starshipsName.innerHTML = data.name;
+
+      starshipsManufacturer = document.createElement('p');
+      starshipsManufacturer.innerHTML = 'Manufacturer: ' + data.manufacturer;
+
+      starshipsClass = document.createElement('p');
+      starshipsClass.innerHTML = 'Class: ' + data.starship_class;
+
+      starshipsUl = document.createElement('ul');
+
+      starshipsContainer.appendChild(starshipsName);
+      starshipsContainer.appendChild(starshipsManufacturer);
+      starshipsContainer.appendChild(starshipsClass);
+      starshipsContainer.appendChild(starshipsUl);
+
+      data.films.forEach( film => {
+        starshipsLi = document.createElement('li');
+        fetch(film)
+        .then( res => {
+          return res.json();
+        })
+        .then( data => {
+          starshipsLi.innerHTML = data.title;
+          starshipsContainer.appendChild(starshipsLi);
+        });
+      });
+
+      if(contentDiv.children.length === 0) {
+        contentDiv.appendChild(starshipsContainer);
+      } else {
+        contentDiv.replaceChild(starshipsContainer, contentDiv.children[0]);
+      }
+    });
   }
 });
